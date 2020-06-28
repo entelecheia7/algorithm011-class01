33ac84bfdfe2c10b7e3071ae0d35c8b1bd9fa78c
entelecheia7 <24566872+entelecheia7@users.noreply.github.com>
2020-06-28 22:21:06 +0800

    update

--- Week_01/preview.go
+++ Week_01/preview.go
@@ -4,6 +4,30 @@ func main() {
 
 }
 
-// 有效的字母异位词
-// 二叉树的中序遍历
-// 最小的 k 个数
+// 1.有效的字母异位词
+func isAnagram(s string, t string) bool {
+	ls, lt := len(s), len(t)
+	if ls != lt {
+		return false
+	}
+	m := make([]int, 26)
+	for i := 0; i < ls; i++ {
+		if s[i] != t[i] {
+			m[s[i]-'a']++
+			m[t[i]-'a']--
+		}
+	}
+	for _, v := range m {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// 法二：给两个字符串按同一算法排序，看生成的字符串是否相等
+// 进阶：将数组换成map，map的下标可以是unicode字符
+
+// 2.二叉树的中序遍历
+
+// 3.最小的 k 个数
