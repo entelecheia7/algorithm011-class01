fb9820734246e84ba19284a61eb42a5ea316db7b
entelecheia7 <24566872+entelecheia7@users.noreply.github.com>
2020-06-29 19:33:27 +0800

    update trapping-rain-water

--- Week_01/homework.go
+++ Week_01/homework.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	nums := []int{1, 2, 3, 4, 5, 6, 7}
-	rotate4(nums, 3)
-	fmt.Println(nums)
+	// 旋转数组
+	// nums := []int{1, 2, 3, 4, 5, 6, 7}
+	// rotate4(nums, 3)
+	// fmt.Println(nums)
+
+	// 接雨水
+	nums := []int{2, 1, 0, 2} // 3
+	fmt.Println(trap3(nums))
 }
 
 /* 简单 */
@@ -186,8 +192,118 @@ func plusOne(digits []int) []int {
 
 /* 困难 */
 // 9.接雨水（亚马逊、字节跳动、高盛集团、Facebook 在半年内面试常考）
+// 法一：按列求。
+// 对于每一个列，如果它左侧和右侧最高的墙比它高，这个列就有雨水
+// O(n)
+func trap(height []int) int {
+	n := len(height)
+	if n < 3 {
+		return 0
+	}
+	// 先计算出对于每一列，它左侧和右侧最高的墙，避免在循环中求解
+	maxLeft := make([]int, n)
+	maxLeft[0] = math.MinInt64
+	for i := 1; i < n; i++ {
+		maxLeft[i] = getMax(maxLeft[i-1], height[i-1])
+	}
+	maxRight := make([]int, n)
+	maxRight[n-1] = math.MinInt64
+	for i := n - 2; i >= 0; i-- {
+		maxRight[i] = getMax(maxRight[i+1], height[i+1])
+	}
+
+	capacity := 0
+	for i := 1; i < n-1; i++ {
+		min := getMin(maxRight[i], maxLeft[i])
+		if min > height[i] {
+			capacity += min - height[i]
+		}
+	}
+
+	return capacity
+}
+
+// 法二：对法一的优化，maxLeft 和 maxRight 不使用数组
+// 使用双指针，从两端计算
+// 当maxLeft小于maxRight时，从左侧计算
+// 当maxRight小于maxLeft时，从右侧计算
+// 从左计算，mexLeft是准确的。对于一个列来说，如果它的maxLeft < height[n-1]，那么它的maxLeft和maxRight的最小值一定在左侧
+// 从右计算，maxRight是准确的。对于一个列来说，如果它的maxRight < height[0]，那么它的maxLeft和maxRight的最小值一定在右侧
+// 时间O(n)，空间O(1)
+func trap2(height []int) int {
+	n := len(height)
+	if n < 3 {
+		return 0
+	}
+	maxLeft := height[0]    // left指针左侧最大元素
+	maxRight := height[n-1] // right指针右侧最大元素
+	capacity := 0
+	left, right := 1, n-2
+	for left <= right {
+		if maxLeft <= maxRight {
+			min := maxLeft
+			if min > height[left] {
+				capacity += min - height[left]
+			}
+			maxLeft = getMax(maxLeft, height[left])
+			left++
+		} else {
+			min := maxRight
+			if min > height[right] {
+				capacity += min - height[right]
+			}
+			maxRight = getMax(maxRight, height[right])
+			right--
+		}
+	}
+
+	return capacity
+}
+
+// 法三：利用栈
+// 遍历高度小于栈顶高度时，说明有积水，入栈
+// 遍历高度大于栈顶高度时，出栈计算
+// 使用这个方法是逐层计算雨水容量的
+// O(n)
+func trap3(height []int) int {
+	n := len(height)
+	if n < 3 {
+		return 0
+	}
+	stack := []int{0}
+	capacity := 0
+	for i := 1; i < n; i++ {
+		for len(stack) != 0 && height[stack[len(stack)-1]] < height[i] { // 找到一个右侧的墙height[i]
+			bottom := stack[len(stack)-1] // 容器底高
+			stack = stack[:len(stack)-1]
+			for len(stack) != 0 && height[stack[len(stack)-1]] == height[bottom] {
+				stack = stack[:len(stack)-1]
+			}
+			if len(stack) > 0 { // 如果找到一个左侧的墙
+				left := stack[len(stack)-1]
+				capacity += (getMin(height[i], height[left]) - height[bottom]) * (i - left - 1) // height*width
+			}
+		}
+		stack = append(stack, i)
+	}
+	return capacity
+}
 
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+func getMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func getMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
