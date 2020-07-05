aec8450105915d6a8ec7adeb36dcb2916919752e
entelecheia7 <24566872+entelecheia7@users.noreply.github.com>
2020-07-06 07:59:55 +0800

    update 22

--- Week_02/preview.go
+++ Week_02/preview.go
@@ -5,7 +5,9 @@ import (
 )
 
 func main() {
-	fmt.Println(climbStairs(5))
+	// fmt.Println(climbStairs(5))
+
+	fmt.Println(generateParenthesis2(3))
 }
 
 // 爬楼梯
@@ -22,6 +24,48 @@ func climbStairs(n int) int {
 }
 
 // 括号生成
+// 法一：回溯
+// 法二：动态规划
+func generateParenthesis1(n int) (result []string) {
+	if n <= 0 {
+		return nil
+	}
+	generateParenthesisHelper("(", &result, n-1, n)
+	return result
+}
+func generateParenthesisHelper(cur string, result *[]string, left, right int) {
+	if left == 0 && right == 0 {
+		*result = append(*result, cur)
+		return
+	}
+	if left > 0 {
+		generateParenthesisHelper(cur+"(", result, left-1, right)
+	}
+	if right > left {
+		generateParenthesisHelper(cur+")", result, left, right-1)
+	}
+}
+func generateParenthesis2(n int) []string {
+	if n <= 0 {
+		return nil
+	}
+	state := make([][]string, n+1)
+	state[0] = []string{""}
+	for i := 1; i <= n; i++ {
+		cur := []string{}
+		for j := 0; j < i; j++ {
+			subState1 := state[j]
+			subState2 := state[i-j-1]
+			for _, str1 := range subState1 {
+				for _, str2 := range subState2 {
+					cur = append(cur, "("+str1+")"+str2)
+				}
+			}
+		}
+		state[i] = cur
+	}
+	return state[n]
+}
 
 // Pow(x, n)
 // 子集
