d6a28d2dd36cf276e7b639bad4727197e160362d
entelecheia7 <24566872+entelecheia7@users.noreply.github.com>
2020-07-18 23:18:37 +0800

    u

--- Week_04/homework.go
+++ Week_04/homework.go
@@ -8,7 +8,10 @@ import (
 func main() {
 	// fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4})) // 7
 
-	fmt.Println(findContentChildren([]int{1, 2, 3}, []int{1, 1})) // 1
+	// fmt.Println(findContentChildren([]int{1, 2, 3}, []int{1, 1})) // 1
+
+	fmt.Println(findDisorderIndex([]int{4, 5, 6, 7, 0, 1, 2})) // 4
+	fmt.Println(findDisorderIndex([]int{4, 5, 7, 0, 1, 2}))    // 3
 }
 
 /* 简单 */
@@ -81,12 +84,122 @@ func findContentChildren(g []int, s []int) (satisfy int) {
 }
 
 // 模拟行走机器人
+func robotSim(commands []int, obstacles [][]int) (area int) {
+	n := len(commands)
+	if n == 0 {
+		return 0
+	}
+	x, y := 0, 0
+	obstaclesMap := make(map[[2]int]bool) // 将障碍物转换为map，加速查找
+	for _, v := range obstacles {
+		obstaclesMap[[2]int{v[0], v[1]}] = true
+	}
+	// 0北 1东 2南 3西
+	direction := 0
+	directionX := [4]int{0, 1, 0, -1} // 朝向对应的x坐标变化
+	directionY := [4]int{1, 0, -1, 0} // 朝向对应的y坐标变化
+
+	for i := 0; i < n; i++ {
+		if commands[i] == -2 {
+			direction = (direction + 3) % 4
+		} else if commands[i] == -1 {
+			direction = (direction + 1) % 4
+		} else {
+			for j := 0; j < commands[i]; j++ {
+				tmpX, tmpY := x+directionX[direction], y+directionY[direction]
+				if _, exist := obstaclesMap[[2]int{tmpX, tmpY}]; exist {
+					break
+				}
+				x, y = tmpX, tmpY
+				// 更新面积
+				area = getMax(area, x*x+y*y)
+			}
+		}
+	}
+	return area
+}
+
+func getMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 // 使用二分查找，寻找一个半有序数组 [4, 5, 6, 7, 0, 1, 2] 中间无序的地方
-// 说明：同学们可以将自己的思路、代码写在第 4 周的学习总结中
+func findDisorderIndex(nums []int) int {
+	left, right := 0, len(nums)-1
+	if right <= 0 {
+		return -1
+	}
+	for left < right {
+		if right-left == 1 {
+			if nums[left] > nums[right] {
+				return right
+			} else {
+				return -1
+			}
+		}
+		mid := left + ((right - left) >> 1)
+		if nums[left] <= nums[mid] {
+			left = mid
+		} else if nums[mid] <= nums[right] {
+			right = mid
+		}
+	}
+	return -1
+}
 
 /* 中等 */
 
 // 单词接龙（亚马逊在半年内面试常考）
+func ladderLength(beginWord string, endWord string, wordList []string) (pathLen int) {
+	n := len(beginWord)
+	wordListMap := make(map[string]bool, len(wordList))
+	for _, word := range wordList {
+		wordListMap[word] = true
+	}
+	if !wordListMap[endWord] {
+		return 0
+	}
+	visitedFromBegin := map[string]bool{beginWord: true}
+	visitedFromEnd := map[string]bool{endWord: true}
+	beginQ, endQ := []string{beginWord}, []string{endWord}
+	pathLen = 1
+	for len(beginQ) > 0 && len(endQ) > 0 {
+		if len(beginQ) > len(endQ) { // 选择一个较短的队列进行后续逻辑
+			beginQ, endQ = endQ, beginQ
+			visitedFromBegin, visitedFromEnd = visitedFromEnd, visitedFromBegin
+		}
+		pathLen++
+		size := len(beginQ)
+		var k byte
+		for i := 0; i < size; i++ {
+			cur := []byte(beginQ[i])
+			for j := 0; j < n; j++ {
+				old := cur[j]
+				for k = 'a'; k <= 'z'; k++ {
+					if cur[j] != k {
+						cur[j] = k
+						nextWod := string(cur)
+						if wordListMap[nextWod] && !visitedFromBegin[nextWod] {
+							if visitedFromEnd[nextWod] {
+								return pathLen
+							}
+							beginQ = append(beginQ, nextWod)
+							visitedFromBegin[nextWod] = true
+						}
+					}
+				}
+				cur[j] = old
+			}
+		}
+		beginQ = beginQ[size:]
+	}
+	return 0
+
+}
+
 // 岛屿数量（近半年内，亚马逊在面试中考查此题达到 350 次）
 // 扫雷游戏（亚马逊、Facebook 在半年内面试中考过）
 // 跳跃游戏 （亚马逊、华为、Facebook 在半年内面试中考过）
