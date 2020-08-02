078fcb2f88a9f91f133a0c2d50e6e2d04536d34c
entelecheia7 <24566872+entelecheia7@users.noreply.github.com>
2020-08-02 23:20:26 +0800

    u

--- Week_06/homework.go
+++ Week_06/homework.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sort"
 )
 
@@ -24,10 +25,6 @@ func main() {
 	fmt.Println(leastInterval([]byte{'A', 'A', 'A', 'B', 'B', 'B'}, 2)) // 8
 
 	// 矩形区域不超过 K 的最大数值和
-
-	// 青蛙过河
-
-	// 分割数组的最大值
 
 	// 学生出勤记录 II
 
@@ -360,8 +357,109 @@ func minDistance(word1 string, word2 string) int {
 // 矩形区域不超过 K 的最大数值和（谷歌在半年内面试中考过）
 
 // 青蛙过河（亚马逊、苹果、字节跳动在半年内面试中考过）
+// 法一：动态规划
+// dp[i][j]表示在从某个位置 x 跳 j 步 是否可达stones[i]（1<=j<=i）
+// 则到达位置 x 的步数为 j-1 || j || j+1
+// dp[i][j] = dp[x][k-1] || dp[x][j] || dp[x][j+1]
+// O(n^2)
+func canCross(stones []int) bool {
+	n := len(stones)
+	if n == 2 {
+		if stones[1] != 1 {
+			return false
+		}
+		return true
+	}
+	dp := make([][]bool, n)
+	for k := range dp {
+		dp[k] = make([]bool, n+1) // +1是为了保证 j+1 步不越界
+	}
+	dp[1][1] = true
+	for i := 2; i < n; i++ {
+		// 本次循环求解 dp[i]，stones[i]可以从 1~i-1 位置抵达，列举所有可能
+		for x := 1; x < i; x++ {
+			// 从 stones[x] 跳到 stones[i]需要多少步
+			needStep := stones[i] - stones[x]
+			// 如果从 stones[x] 跳 needStep 步可达 stones[i]，则有 dp[x][needStep] 或 dp[x][needStep-1] 或 dp[x][needStep+1] 为 true
+			if needStep <= i {
+				dp[i][needStep] = dp[x][needStep] || dp[x][needStep-1] || dp[x][needStep+1]
+				if i == n-1 && dp[i][needStep] {
+					return true
+				}
+			}
+		}
+
+	}
+
+	return false
+}
 
 // 分割数组的最大值（谷歌、亚马逊、Facebook 在半年内面试中考过）
+// 法一：动态规划，O(n^3)
+// dp[i][j]表示以将nums的前i个数分为 j 组得到的最大连续子数组和的最小值(j<=i)
+// 设前 k 个数分为 j-1 组，最后一组为第 k+1 到第 i 个数 (k+1>=j)
+func splitArray1(nums []int, m int) (result int) {
+	n := len(nums)
+	dp := make([][]int, n+1)
+	// init
+	for k := range dp {
+		dp[k] = make([]int, m+1)
+		for j := 0; j <= m; j++ {
+			dp[k][j] = math.MaxInt64
+		}
+	}
+	dp[0][0] = 0
+	// sums 表示前i个元素的和
+	sums := make([]int, n+1)
+	for i := 0; i < n; i++ {
+		sums[i+1] = sums[i] + nums[i]
+	}
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m && j <= i; j++ {
+			for k := 0; k < i; k++ {
+				dp[i][j] = getMin(dp[i][j], getMax(sums[i]-sums[k], dp[k][j-1]))
+			}
+		}
+	}
+
+	return dp[n][m]
+}
+
+// 法二：二分查找
+// best
+// nums子数组的值在 [max(nums), sum(nums)]中间，使用二分查找不断查找接近值
+func splitArray2(nums []int, m int) (result int) {
+	n := len(nums)
+	left := nums[0]
+	right := nums[0]
+	for i := 1; i < n; i++ {
+		right += nums[i]
+		if nums[i] > left {
+			left = nums[i]
+		}
+	}
+	for left < right {
+		mid := left + ((right - left) >> 1)
+		// 从nums[0]开始计算一个 subSum
+		// 每找出一个subSum <= mid 的子数组，统计数量+1
+		count := 1 // 循环中subSum超过mid才进行统计，因此最后一轮没有在循环中统计，所以赋1
+		subSum := 0
+		for i := 0; i < n; i++ {
+			subSum += nums[i]
+			if subSum > mid {
+				count++
+				subSum = nums[i]
+			}
+		}
+		if count > m { // 分组数量过多，说明选择的subSum过小
+			left = mid + 1
+		} else { // 分组数量小于等于m，说明选择的subSum可能过大
+			right = mid
+		}
+	}
+
+	return left
+}
 
 // 学生出勤记录 II （谷歌在半年内面试中考过）
 
