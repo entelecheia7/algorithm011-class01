01654c055654e4fdc279d8cdb5bacc3be3de2206
entelecheia7 <24566872+entelecheia7@users.noreply.github.com>
2020-08-25 16:21:33 +0800

    u

--- Week_09/homework.go
+++ Week_09/homework.go
@@ -8,12 +8,10 @@ import (
 func main() {
 	fmt.Println(validPalindrome2("abca"))
 
-	// 在学习总结中，写出不同路径 2 这道题目的状态转移方程。
 	// 最长上升子序列（字节跳动、亚马逊、微软在半年内面试中考过）
 	// 解码方法（字节跳动、亚马逊、Facebook 在半年内面试中考过）
 	// 字符串转换整数 (atoi) （亚马逊、微软、Facebook 在半年内面试中考过）
 	// 找到字符串中所有字母异位词（Facebook 在半年内面试中常考）
-	// 最长回文子串（亚马逊、字节跳动、华为在半年内面试中常考）
 
 	// 最长有效括号（亚马逊、字节跳动、华为在半年内面试中考过）
 	// 赛车（谷歌在半年内面试中考过）
@@ -239,13 +237,73 @@ func validPalindrome2(s string) (result bool) {
 }
 
 /* 中等 */
-
-// 在学习总结中，写出不同路径 2 这道题目的状态转移方程。
 // 最长上升子序列（字节跳动、亚马逊、微软在半年内面试中考过）
 // 解码方法（字节跳动、亚马逊、Facebook 在半年内面试中考过）
 // 字符串转换整数 (atoi) （亚马逊、微软、Facebook 在半年内面试中考过）
 // 找到字符串中所有字母异位词（Facebook 在半年内面试中常考）
 // 最长回文子串（亚马逊、字节跳动、华为在半年内面试中常考）
+// 法一：暴力+双指针
+// best
+func longestPalindrome(s string) (result string) {
+	n := len(s)
+	if n == 0 {
+		return
+	}
+	result = s[:1]
+	for i := 1; i < n-len(result)/2; i++ {
+		// 以s[i]为奇数长度回文串的中心
+		left, right := i-1, i+1
+		for left >= 0 && right < n && s[left] == s[right] {
+			left--
+			right++
+		}
+		if left < i-1 && right-left-1 > len(result) {
+			result = s[left+1 : right]
+		}
+		// 以s[i]为偶数长度回文串的右中心
+		left, right = i-1, i
+		for left >= 0 && right < n && s[left] == s[right] {
+			left--
+			right++
+		}
+		if right-left > 1 && right-left-1 > len(result) {
+			result = s[left+1 : right]
+		}
+	}
+	return result
+}
+
+// 法二：动态规划
+// dp[i][j]表示s[i:j]是否为回文字符串，含两侧边界
+// O(n^2)
+// 该解法还可以进行优化，dp[i][j]仅和sp[i+1][j-1]相关
+func longestPalindrome2(s string) (result string) {
+	n := len(s)
+	if n <= 1 {
+		return s
+	}
+	result = s[:1]
+	dp := make([][]bool, n)
+	for k := range dp {
+		dp[k] = make([]bool, n)
+		dp[k][k] = true
+	}
+	for j := 1; j < n; j++ {
+		for i := 0; i < j; i++ {
+			if s[i] == s[j] {
+				if j == i+1 {
+					dp[i][j] = true
+				} else if dp[i+1][j-1] {
+					dp[i][j] = true
+				}
+				if dp[i][j] && j-i+1 > len(result) {
+					result = s[i : j+1]
+				}
+			}
+		}
+	}
+	return result
+}
 
 /* 困难 */
 // 最长有效括号（亚马逊、字节跳动、华为在半年内面试中考过）
@@ -256,6 +314,13 @@ func validPalindrome2(s string) (result bool) {
 /* helper */
 func getMin(a, b int) int {
 	if a < b {
+		return a
+	}
+	return b
+}
+
+func getMax(a, b int) int {
+	if a > b {
 		return a
 	}
 	return b
